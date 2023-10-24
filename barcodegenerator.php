b837a16aa017628c681f46f86a9a0d7b440ab1c6
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2023-10-24 10:43:14 -0400

    change version

--- barcodegenerator.php
+++ barcodegenerator.php
@@ -40,7 +40,7 @@ class Barcodegenerator extends Module
     {
         $this->name = 'barcodegenerator';
         $this->tab = 'market_place';
-        $this->version = '1.0.4';
+        $this->version = '1.0.5';
         $this->author = 'cdigruttola';
         $this->need_instance = 0;
         $this->module_key = '05df11732203e6e6bcdb690348257aa7';
